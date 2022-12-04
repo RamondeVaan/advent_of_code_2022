fc9a5f0bc2bc0f05406a4f424dc6a01c56eec42e
Ramon de Vaan <ramon.de.vaan@outlook.com>
2022-12-04 13:02:48 +0100

    Prepare Day05

---
+++ app/src/main/java/nl/ramondevaan/aoc2022/day05/Day05.java
@@ -0,0 +1,17 @@
+package nl.ramondevaan.aoc2022.day05;
+
+import java.util.List;
+
+public class Day05 {
+
+  public Day05(final List<String> lines) {
+  }
+
+  public long solve1() {
+    return 0;
+  }
+
+  public long solve2() {
+    return 0;
+  }
+}
