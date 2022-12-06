c917c8a0f6249b4beef7a3ec957f4115afda659c
Ramon de Vaan <ramon.de.vaan@outlook.com>
2022-12-06 11:23:37 +0100

    Day06

--- app/src/main/java/nl/ramondevaan/aoc2022/day06/Day06.java
+++ app/src/main/java/nl/ramondevaan/aoc2022/day06/Day06.java
@@ -4,14 +4,27 @@ import java.util.List;
 
 public class Day06 {
 
+  private final String datastreamBuffer;
+
   public Day06(final List<String> lines) {
+    this.datastreamBuffer = lines.get(0);
   }
 
   public long solve1() {
-    return 0;
+    return solve(4);
   }
 
   public long solve2() {
-    return 0;
+    return solve(14);
+  }
+
+  public long solve(int window) {
+    for (int from = 0, to = window; to <= datastreamBuffer.length(); from++, to++) {
+      if (datastreamBuffer.substring(from, to).chars().distinct().count() == window) {
+        return to;
+      }
+    }
+
+    throw new IllegalArgumentException();
   }
 }
--- app/src/test/java/nl/ramondevaan/aoc2022/day06/Day06Test.java
+++ app/src/test/java/nl/ramondevaan/aoc2022/day06/Day06Test.java
@@ -25,12 +25,12 @@ class Day06Test {
 
   @Test
   void puzzle1() {
-    assertEquals(0L, day06.solve1());
+    assertEquals(1287L, day06.solve1());
   }
 
   @Test
   void puzzle2() {
-    assertEquals(0L, day06.solve2());
+    assertEquals(3716L, day06.solve2());
   }
 
 }
