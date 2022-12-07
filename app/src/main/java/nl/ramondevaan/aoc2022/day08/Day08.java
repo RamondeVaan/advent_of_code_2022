f3692b32194a5df69d50a55aa482a935d75f2d2f
Ramon de Vaan <ramon.de.vaan@outlook.com>
2022-12-07 22:31:36 +0100

    Prepare Day08

---
+++ app/src/main/java/nl/ramondevaan/aoc2022/day08/Day08.java
@@ -0,0 +1,18 @@
+package nl.ramondevaan.aoc2022.day08;
+
+import java.util.List;
+
+public class Day08 {
+
+  public Day08(final List<String> lines) {
+
+  }
+
+  public long solve1() {
+    return 0;
+  }
+
+  public long solve2() {
+    return 0;
+  }
+}
