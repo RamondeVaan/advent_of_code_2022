73c4f839042334cda5c0eba349c244f5bed47c74
Ramon de Vaan <ramon.de.vaan@outlook.com>
2022-12-18 02:39:34 +0100

    Refactor Day17

--- app/src/main/java/nl/ramondevaan/aoc2022/day17/Day17.java
+++ app/src/main/java/nl/ramondevaan/aoc2022/day17/Day17.java
@@ -4,20 +4,16 @@ import nl.ramondevaan.aoc2022.util.Cycle;
 import nl.ramondevaan.aoc2022.util.IntMap;
 import nl.ramondevaan.aoc2022.util.MutableIntMap;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Day17 {
 
   private final List<IntMap> rockTypes;
   private final List<Jet> jets;
-  private final int rockBits;
 
   public Day17(final List<String> lines) {
     final var rockTypeParser = new RockTypeParser();
     this.rockTypes = rockTypeParser.parse(Day17.class.getResource("/day_17_rock_types.txt"));
-    this.rockBits = Integer.SIZE - Integer.numberOfLeadingZeros(rockTypes.size());
 
     final var jetParser = new JetParser();
     this.jets = jetParser.parse(lines);
@@ -29,6 +25,37 @@ public class Day17 {
 
   public long solve2() {
     return solve(1_000_000_000_000L);
+  }
+
+  public long solve(final long rounds) {
+    final var map = new MutableIntMap(10_000, 7);
+    final var rockCycle = new Cycle<>(rockTypes);
+    final var jetCycle = new Cycle<>(jets);
+    final var states = new HashMap<Integer, List<State>>();
+
+    var height = 0;
+    var heightOffset = 0L;
+    var round = 0L;
+    for (;round < rounds; round++) {
+      final var state = getCycle(map, height, round, states, rockCycle, jetCycle);
+      if (state != null) {
+        final var length = round - state.round();
+        final var remaining = rounds - round;
+        final var times = remaining / length;
+        final var heightDiff = height - state.height();
+        heightOffset += times * heightDiff;
+        round += length * times;
+        break;
+      }
+      final var rock = rockCycle.next();
+      height = Math.max(rock.rows() + drop(map, rock, jetCycle, height + 3), height);
+    }
+    for (;round < rounds; round++) {
+      final var rock = rockCycle.next();
+      height = Math.max(rock.rows() + drop(map, rock, jetCycle, height + 3), height);
+    }
+
+    return height + heightOffset;
   }
 
   private int drop(final MutableIntMap map, final IntMap rock, final Cycle<Jet> jetCycle, final int height) {
@@ -77,53 +104,37 @@ public class Day17 {
     }
   }
 
-  public long solve(final long rounds) {
-    final var map = new MutableIntMap(10_000, 7);
-    final var rockCycle = new Cycle<>(rockTypes);
-    final var jetCycle = new Cycle<>(jets);
-    final var states = new HashMap<Integer, State>();
-
-    var height = 0;
-    var heightOffset = 0L;
-    var round = 0L;
-    for (;round < rounds; round++) {
-      final var rock = rockCycle.next();
-      height = Math.max(rock.rows() + drop(map, rock, jetCycle, height + 3), height);
-      final var state = getCycle(map, height, round, states, rockCycle, jetCycle);
-      if (state != null) {
-        final var length = round - state.round();
-        final var remaining = rounds - round;
-        final var times = remaining / length;
-        final var heightDiff = height - state.height();
-        heightOffset += times * heightDiff;
-        round += length * times + 1;
-        break;
-      }
-    }
-    for (;round < rounds; round++) {
-      final var rock = rockCycle.next();
-      height = Math.max(rock.rows() + drop(map, rock, jetCycle, height + 3), height);
-    }
-
-    return height + heightOffset;
-  }
-
-  private State getCycle(final MutableIntMap map, final int height, long round, final Map<Integer, State> states,
+  private State getCycle(final MutableIntMap map, final int height, long round, final Map<Integer, List<State>> states,
                          final Cycle<IntMap> rockCycle, final Cycle<Jet> jetCycle) {
-    final var topRow = map.row(height - 1);
-    for (final var value : topRow) {
-      if (value != 1) {
-        return null;
+    final var depthByColumn = new ArrayList<Integer>(map.columns());
+
+    outer: for (int column = 0; column < map.columns(); column++) {
+      for (int i = height - 1, depth = 0; i >= 0; i--, depth++) {
+        if(map.valueAt(i, column) > 0) {
+          depthByColumn.add(depth);
+          continue outer;
+        }
       }
+      depthByColumn.add(height);
     }
+
     final var rockIndex = rockCycle.nextIndex();
     final var jetIndex = jetCycle.nextIndex();
-    final var id = (jetIndex << rockBits) | rockIndex;
-    final var state = states.get(id);
-    if (state != null) {
-      return state;
+    int id = Objects.hash(depthByColumn, rockIndex, jetIndex);
+    final var newState = new State(depthByColumn, round, height, rockIndex, jetIndex);
+    final var list = states.get(id);
+    if (list != null) {
+      for (final var state : list) {
+        if (state.equals(newState)) {
+          return state;
+        }
+      }
+      list.add(newState);
+    } else {
+      final var newList = new ArrayList<State>();
+      newList.add(newState);
+      states.put(id, newList);
     }
-    states.put(id, new State(round, height, rockIndex, jetIndex));
     return null;
   }
 }
--- app/src/main/java/nl/ramondevaan/aoc2022/day17/State.java
+++ app/src/main/java/nl/ramondevaan/aoc2022/day17/State.java
@@ -1,4 +1,19 @@
 package nl.ramondevaan.aoc2022.day17;
 
-public record State(long round, long height, int rockCycle, int jetCycle) {
+import java.util.List;
+import java.util.Objects;
+
+public record State(List<Integer> depths, long round, long height, int rockCycle, int jetCycle) {
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final State state = (State) o;
+    return rockCycle == state.rockCycle && jetCycle == state.jetCycle && depths.equals(state.depths);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(depths, rockCycle, jetCycle);
+  }
 }
