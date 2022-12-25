08f1823fcd216680119ec607ecaae13f3cd395a0
Ramon de Vaan <ramon.de.vaan@outlook.com>
2022-12-25 01:06:53 +0100

    Prepare Day25

---
+++ app/src/test/java/nl/ramondevaan/aoc2022/day25/Day25Test.java
@@ -0,0 +1,36 @@
+package nl.ramondevaan.aoc2022.day25;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day25Test {
+
+  static Day25 day25;
+
+  @BeforeAll
+  static void setUp() throws URISyntaxException, IOException {
+    Path path = Path.of(Objects.requireNonNull(Day25Test.class.getResource("/input/day_25.txt")).toURI());
+    List<String> lines = Files.readAllLines(path);
+    day25 = new Day25(lines);
+  }
+
+  @Test
+  void puzzle1() {
+    assertEquals(0L, day25.solve1());
+  }
+
+  @Test
+  void puzzle2() {
+    assertEquals(0L, day25.solve2());
+  }
+
+}
